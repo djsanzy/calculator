ea43676a195054c06590a8387fd5494a21bd36c6
code18 <79305021+djsanzy@users.noreply.github.com>
2023-02-20 18:33:34 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,62 @@
+
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+def calculator():
+    num1 = float(input("What's the first number?: \n"))
+    still_calculating = True
+
+    while still_calculating:
+        for symbol in operations:
+            print(symbol)
+        operation_choice = input("Pick an operation: \n")
+        num2 = float(input("What's the next number?: \n"))
+        operation_func = operations[operation_choice]
+        answer = operation_func(num1, num2)
+
+        user_choice = input(f"Type 'y' to continue with {answer}, or type 'n' to start a new calculator: \n").lower()
+
+        if user_choice == 'y':
+            num1 = answer
+        elif user_choice == 'n':
+            calculator()
+        else:
+            print("Wrong Input")
+            still_calculating = False
+
+
+calculator()
